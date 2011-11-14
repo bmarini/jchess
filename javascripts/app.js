12f9292feba1884cf69ca6b9a33054f9a64c590c
Ben Marini <bmarini@gmail.com>
2011-11-14 09:49:21 -0800

    Cleanup example code a bit

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,70 +1,32 @@
-jQuery(document).ready(function() {
-  var chess1 = jQuery('#board1').chess();
-  var chess2 = jQuery('#board2').chess({fen : "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"});
-  var chess3 = jQuery('#board3').chess({pgn : jQuery('#pgn-fischer-spassky').html()});
-  var chess4 = jQuery('#board4').chess({pgn : jQuery('#justdoeet').html()});
-  var chess5 = jQuery('#board5').chess({pgn : jQuery('#with-queening').html()});
-  var chess6 = jQuery('#board6').chess({pgn : jQuery('#unambiguous-knight-move').html()});
+jQuery(function($) {
+  function loadChessGame(container, options, callback) {
+    var chess = $('.board', container).chess(options);
 
-  jQuery('#board3-back').click(function() {
-    chess3.transitionBackward();
-    jQuery("#board3-annot").text( chess3.annotation() );
-    return false;
-  });
-  
-  jQuery('#board3-next').click(function() {
-    chess3.transitionForward();
-    jQuery("#board3-annot").text( chess3.annotation() );
-    return false;
-  });
-  
-  jQuery('#board3-flip').click(function() {
-    chess3.flipBoard();
-    return false;
-  });  
+    $('.back', container).click(function() {
+      chess.transitionBackward();
+      $('.annot', container).text( chess.annotation() );
+      return false;
+    });
 
-  jQuery('#board4-back').click(function() {
-    chess4.transitionBackward();
-    return false;
-  });
-  
-  jQuery('#board4-next').click(function() {
-    chess4.transitionForward();
-    return false;
-  });
-  
-  jQuery('#board4-flip').click(function() {
-    chess4.flipBoard();
-    return false;
-  }); 
+    $('.next', container).click(function() {
+      chess.transitionForward();
+      $('.annot', container).text( chess.annotation() );
+      return false;
+    });
 
-  jQuery('#board5-back').click(function() {
-    chess5.transitionBackward();
-    return false;
-  });
-  
-  jQuery('#board5-next').click(function() {
-    chess5.transitionForward();
-    return false;
-  });
-  
-  jQuery('#board5-flip').click(function() {
-    chess5.flipBoard();
-    return false;
-  }); 
-  
-  jQuery('#board6-back').click(function() {
-    chess6.transitionBackward();
-    return false;
-  });
-  
-  jQuery('#board6-next').click(function() {
-    chess6.transitionForward();
-    return false;
-  });
-  
-  jQuery('#board6-flip').click(function() {
-    chess6.flipBoard();
-    return false;
-  });  
-})
+    $('.flip', container).click(function() {
+      chess.flipBoard();
+      return false;
+    });
+
+    if ( typeof callback != "undefined" ) { callback(chess) };
+  }
+
+  loadChessGame( '#game1', {} );
+  loadChessGame( '#game2', { fen : "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2" } );
+  loadChessGame( '#game3', { pgn : $('#pgn-fischer-spassky').html() } );
+  loadChessGame( '#game4', { pgn : $('#justdoeet').html() } );
+  loadChessGame( '#game5', { pgn : $('#with-queening').html() } );
+  loadChessGame( '#game6', { pgn : $('#unambiguous-knight-move').html() } );
+
+});
